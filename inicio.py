e547463fe1284433520ca289a10042a9cee17ad7
Carolina2118 <loboscarolinaa@gmail.com>
2025-03-03 11:05:34 -0300

    variable semántica,sintaxis y comentarios

---
+++ inicio.py
@@ -0,0 +1,11 @@
+print("HOLA") #("buena practica")
+print("MUNDO") #("error al agregar 3 espacios")
+""" comentarios largo 
+largo"""
+saludo = ("Hola")
+saludo = ("Hi")
+# se ejecuta de arriba a bajo 
+# de izquierda a derecha 
+Nombre = ("Carito")
+print(saludo)
+print(Nombre)
