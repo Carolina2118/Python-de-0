771aaedee04affc94a75344ee12d56464c47ac6c
Carolina2118 <loboscarolinaa@gmail.com>
2025-03-03 18:00:29 -0300

    Enteros, Flotantes y Booleanos

---
+++ FundamentosdeProgramación/clase6.py
@@ -0,0 +1,21 @@
+#flotante: float  el punto es flotante ej; X = 1.2E6 Son todos los números decimales. 
+# Enteros: Son todos los números enteros. Se representa con la palabra Integer (INT))
+# Booleanos: Representa verdadero (true) o falso (false).
+# PARA PREGUNTAR EL TIPO DE DATO EN PRINT SE AGREGA TYPE.
+
+x = 10
+y = 5.678
+z = 1.2E6 #exponencial
+a = 1.2E-6
+
+print(type(x))
+print(type(y))
+print(z)
+print(a) 
+print(x + y)
+print(x + x)
+print(y + y)
+is_true = True
+is_false = False
+print((is_true))
+print(type(is_false))
