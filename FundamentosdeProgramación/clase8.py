fb074e686c91fe25f02514e8b742e72ca16f626e
Carolina2118 <loboscarolinaa@gmail.com>
2025-03-03 18:51:34 -0300

    Operaciones Matemáticas

---
+++ FundamentosdeProgramación/clase8.py
@@ -0,0 +1,25 @@
+#Operaciones Numericas
+a = 10
+b = 10
+print("Suma:", a + b)
+print("Resta:", a - b)
+print("Multiplicación:", a * b)
+print("Potenciación:", a ** b)
+print("División:", a / b)
+print("Parte entera de la división:", a // b)
+print("Módulo:", a % b)
+#usaremos la variable a 
+# atajos o shortcuts
+#a = a + 2 
+#a += 2
+#a *= 2
+a /= 2
+
+print(a)
+# PEMDAS
+operation_1  = 2 + 3 * 4
+operation_2  = (2 + 3) * 4
+print(operation_1)
+print(operation_2)
+operation_3 = (2+3) * (4**2)/ 8 - 1
+print(operation_3)
