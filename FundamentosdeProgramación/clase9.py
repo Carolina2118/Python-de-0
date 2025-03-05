39dc21404c99463d3e44cf229c9a7544198cf7a6
Carolina2118 <loboscarolinaa@gmail.com>
2025-03-05 17:46:46 -0300

    operaciones de Entrada/Salida en Consola

---
+++ FundamentosdeProgramación/clase9.py
@@ -0,0 +1,10 @@
+# con input siempre sera str 
+name = input(" ingresa tu nombre:")
+print(type(name))
+age = input("ingresa tu edad:")
+print(type(age))
+# si agregamos int cambia su valor 
+nombre = input("ingresa nombre: ")
+print(type(nombre))
+edad = int(input("ingresa tu edad:"))
+print(type(edad))
