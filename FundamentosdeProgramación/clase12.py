c32fa12eaf4f87557f5a97ff1b592709e9779242
Carolina2118 <loboscarolinaa@gmail.com>
2025-03-07 18:43:21 -0300

    Listas de más dimensiones y Tuplas

---
+++ FundamentosdeProgramación/clase12.py
@@ -0,0 +1,12 @@
+
+matrix = [[1,2,3],
+          [4,5,6],
+          [7,8,9]]
+print(matrix[2][1])
+numbers = (1,2,3,4,5) # con o sin parentesis lo reconose como tuple
+print(numbers)
+print(type(numbers))
+print(numbers[0])
+numbers[0] = 'unos'
+# si es tuple no permite modificacion 
+print(numbers)
